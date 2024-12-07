a91fd1d256e5465e05dd3ff2302198a5d8b99e58
Rössing,Cornelius (PEG-HAM3) <cornelius.roessing@porsche-engineering.de>
2024-12-07 13:00:25 +0100

    Weather broken?

--- public/js/weatherPopup.js
+++ public/js/weatherPopup.js
@@ -3,5 +3,6 @@ var button = document.getElementById("wetterbutton")
 
 
 button.onclick = function openpopup() {
+    console.log("open weather")
     window.open("/html/weather.html", "PopupWindow")
 }
