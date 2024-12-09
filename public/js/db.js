c0783d366b3ea8f9a6473ad27d7c829ee5d75a45
Rössing,Cornelius (PEG-HAM3) <cornelius.roessing@porsche-engineering.de>
2024-12-09 14:24:05 +0100

    db verspätung and cancelled

--- public/js/db.js
+++ public/js/db.js
@@ -47,7 +47,9 @@ function lastElement(array) {
     return
 }
 
-function createIsland(item, sName) {
+function createIsland(object, sName) {
+    const item = object[0]
+    const changes = object[1]
     var code = ""
     var tripLabel = item.tl[0].$
     var ArDepInd = 0       /*0=ar&dep; 1=ar; 2=dep*/
@@ -74,41 +76,56 @@ function createIsland(item, sName) {
 
     code += "<div class='trainIsland'>"
     code += "<div class='" + tripLabel.c + "'>"
-    switch(ArDepInd) {
-        case 0: {
-            code += "<h4 class='trainName'>" + tripLabel.c + " " + tripLabel.n + " to " + follStops[follStops.length-1] + "</h4>"
-            code += "<p>Platform " + plattform + "</p>"
-            code += "<p>Arrival: " + dbToDate(arrival.pt) + " from " + prevStops[prevStops.length-1] + "</p>"
-            code += "<p>Departure: " + dbToDate(departure.pt) + " to " + follStops[0] + "</p>"
-            code += "<p class='FollowStopsButton'>Following stops</p>"
-            code += "<div class='followStopsClass'>"
-            for(var i in follStops) {
-                code += "<p>" + follStops[i] + "</p>"
+    
+    var cancelled = 0
+    try {
+        cancelled = changes.m[0].$.del
+        console.error({"canceled": tripLabel.c + " " + tripLabel.n})
+    }
+    catch {}
+
+    if(cancelled != 1){
+        switch(ArDepInd) {
+            case 0: {
+                code += "<h4 class='trainName'>" + tripLabel.c + " " + tripLabel.n + " to " + follStops[follStops.length-1] + "</h4>"
+                code += corrPlattform(plattform, changes)
+                code += corrArrival([arrival.pt, prevStops], changes)
+                code += corrDeparture([departure.pt, follStops], changes)
+                code += "<div class='singleInfo'><p class='FollowStopsButton'>Following stops</p>"
+                code += "<div class='followStopsClass'>"
+                for(var i in follStops) {
+                    code += "<p>" + follStops[i] + "</p>"
+                }
+                code += "</div></div>"
+                break
             }
-            code += "</div>"
-            break
-        }
-        case 1: {
-            code += "<h4 class='trainName'>" + tripLabel.c + " " + tripLabel.n + " to " + sName + "</h4>"
-            code += "<p>Platform " + plattform + "</p>"
-            code += "<p>Arrival: " + dbToDate(arrival.pt) + " from " + prevStops[prevStops.length-1] + "</p>"
-            code += "<p>Train ending at " + sName + ".</p>"
-            break
-        }
-        case 2: {
-            code += "<h4 class='trainName'>" + tripLabel.c + " " + tripLabel.n + " to " + follStops[follStops.length-1] + "</h4>"
-            code += "<p>Platform " + plattform + "</p>"
-            code += "<p>Train starting at " + sName + ".</p>"
-            code += "<p>Departure: " + dbToDate(departure.pt) + " to " + follStops[0] + "</p>"
-            code += "<p class='FollowStopsButton'>Following stops</p>"
-            code += "<div class='followStopsClass'>"
-            for(var i in follStops) {
-                code += "<p>" + follStops[i] + "</p>"
+            case 1: {
+                code += "<h4 class='trainName'>" + tripLabel.c + " " + tripLabel.n + " to " + sName + "</h4>"
+                code += corrPlattform(plattform, changes)
+                code += corrArrival([arrival.pt, prevStops], changes)
+                code += "<div class='singleInfo'><p>Train ending at " + sName + "</p></div>"
+                break
             }
-            code += "</div>"
-            break
+            case 2: {
+                code += "<h4 class='trainName'>" + tripLabel.c + " " + tripLabel.n + " to " + follStops[follStops.length-1] + "</h4>"
+                code += corrPlattform(plattform, changes)
+                code += "<div class='singleInfo'><p>Train starting at " + sName + "</p></div>"
+                code += corrDeparture([departure.pt, follStops], changes)
+                code += "<div class='singleInfo'><p class='FollowStopsButton'>Following stops</p>"
+                code += "<div class='followStopsClass'>"
+                for(var i in follStops) {
+                    code += "<p>" + follStops[i] + "</p>"
+                }
+                code += "</div></div>"
+                break
+            }
+            default: break
         }
-        default: break
+    }
+    else {
+        code += "<h4 class='trainName' style='text-decoration:line-through;color:red;'>" + tripLabel.c + " " + tripLabel.n + " to " + follStops[follStops.length-1] + "</h4>"
+        code += "<div class='singleInfo'><p style='color:grey;'>original arrival: " + dbToDate(arrival.pt) + "</p></div>"
+        code += "<div class='singleInfo'><p>Train cancelled</p></div>"
     }
 
     code += "</div></div>"
@@ -116,13 +133,10 @@ function createIsland(item, sName) {
 }
 
 async function getTimetable(evaNo, datestr) {
-    fetch("../db-timetable?evaNo=" + evaNo + "&date=" + datestr[0] + "&hour=" + datestr[1])
+    return await fetch("../db-timetable?evaNo=" + evaNo + "&date=" + datestr[0] + "&hour=" + datestr[1])
         .then(result => result.json())
         .then(data => {
             data = data.timetable
-            console.log(data.s)
-            const traindiv = document.getElementById("trainsid")
-            const stationName = data.$.station
 
             var times = []
             for(var i in data.s) {
@@ -135,11 +149,7 @@ async function getTimetable(evaNo, datestr) {
             }
 
             var sorted = sortTimes(times)
-            traindiv.innerHTML = ""
-            for(var i in sorted) {
-                traindiv.innerHTML += createIsland(data.s[sorted[i][1]], stationName)
-            }
-
+            return [data, sorted]
 
         })
 }
@@ -206,19 +216,108 @@ const datetime = document.getElementById("datetime")
 sButton.onclick = function() {
     var datetimeStr = datetime.value.split("T")
     datetimeStr[0] = datetimeStr[0].replace(/-/g, "").substring(2)
-    datetimeStr[1] = datetimeStr[1].replace(":", "").substring(0, 2)
+    datetimeStr[1] = datetimeStr[1].replace(":", "").substring(0, 2);
 
-    getChanges(stationList[stationSelect.value].evaNumbers[0].number)
-    getTimetable(stationList[stationSelect.value].evaNumbers[0].number, datetimeStr)
+    (async() => {
+        const changes = await getChanges(stationList[stationSelect.value].evaNumbers[0].number)
+        const response = await getTimetable(stationList[stationSelect.value].evaNumbers[0].number, datetimeStr)
+        console.log(changes)
+        console.log(response)
+
+        const traindiv = document.getElementById("trainsid")
+        const stationName = response[0].$.station
+        traindiv.innerHTML = ""
+        var items = response[0].s
+        var objects = []
+        for(var x in items) {
+            objects.push(createObject(items[x], changes))
+        }
+        for(var i in response[1]) {
+            traindiv.innerHTML += createIsland(objects[response[1][i][1]], stationName)
+        }
+    })()
 }
 
 
 /*Changes*/
 async function getChanges(evaNo) {
-    fetch("../db-changes?evaNo=" + evaNo)
+    return await fetch("../db-changes?evaNo=" + evaNo)
         .then(result => result.json())
         .then(data => {
-            data = data.timetable.s
+            return data.timetable.s
         })
 }
 
+function createObject(item, changes) {
+    const identifier = item.$.id
+    var idchanges
+    for(var j in changes) {
+        if(identifier == changes[j].$.id){
+            idchanges = changes[j]
+            changes.splice(j, 1)
+        }
+    }
+
+    return [item, idchanges]
+}
+
+function corrPlattform(planned, changed) {
+    var code = "<div class='singleInfo'>"
+    var newPlattform = 0
+    try {
+        newPlattform = changed.ar[0].$.cp
+        }
+    catch {
+        try{
+            newPlattform = changed.dp[0].$.cp
+        }
+        catch {
+        }
+        
+    }
+    if(newPlattform) {
+        code += "<div><p>Platform </p><p style='text-decoration:line-through;color:red;'>" + planned + "</p><p> " + newPlattform + "</p></div>"
+    }
+    else {
+        code += "<p>Platform " + planned + "</p>"
+    }
+
+    code += "</div>"
+    return code
+}
+
+function corrArrival(planned, changed) {
+    var code = "<div class='singleInfo'>"
+    var newArrival = planned[0]
+    try {
+        newArrival = changed.ar[0].$.ct
+        }
+    catch {}
+        
+    if(newArrival != planned[0]) {
+        code += "<div class='correctionOut'><div style='display:grid;grid-auto-flow:column;grid-auto-columns:max-content;'><p>Arrival:&nbsp</p><p style='text-decoration:line-through;color:red;'>" + dbToDate(planned[0]) + "&nbsp</p><p>" + dbToDate(newArrival) + "&nbsp</p></div><p> from " + planned[1][planned[1].length-1] + "</p></div>"
+    }
+    else {
+        code += "<p>Arrival: " + dbToDate(planned[0]) + " from " + planned[1][planned[1].length-1] + "</p>"
+    }
+    code += "</div>"
+    return code
+}
+
+function corrDeparture(planned, changed) {
+    var code = "<div class='singleInfo'>"
+    var newDeparture = planned[0]
+    try {
+        newDeparture = changed.dp[0].$.ct
+        }
+    catch {}
+        
+    if(newDeparture != planned[0]) {
+        code += "<div class='correctionOut'><div style='display:grid;grid-auto-flow:column;grid-auto-columns:max-content;'><p>Departure:&nbsp</p><p style='text-decoration:line-through;color:red;'>" + dbToDate(planned[0]) + "&nbsp</p><p>" + dbToDate(newDeparture) + "&nbsp</p></div><p>to " + planned[1][0] + "</p></div>"
+    }
+    else {
+        code += "<p>Departure: " + dbToDate(planned[0]) + " to " + planned[1][0] + "</p>"
+    }
+    code += "</div>"
+    return code
+}
